769249675e671071082b8e2d8e884deec8b1e0d5
tqa3001 <tqa3001@gmail.com>
2022-11-29 10:20:49 +1100

    reconfigure package.json and create style.css

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express'); 
 const app = express(); 
-const PORT = 3000; 
+const path = require('path'); 
+const PORT = process.env.PORT || 3000; 
+
+app.use('/', express.static(path.join(__dirname, '/public'))); 
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(path.join(__dirname, '/views/index.html'))
 })
 
 app.listen(PORT, () => {
