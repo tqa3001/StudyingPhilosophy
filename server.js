035041f4b1f26fd61d378d1b3a13a4db1ba98229
tqa3001 <tqa3001@gmail.com>
2022-11-29 11:21:11 +1100

    added 404 page and logs

--- server.js
+++ server.js
@@ -5,9 +5,18 @@ const PORT = process.env.PORT || 3000;
 
 app.use('/', express.static(path.join(__dirname, '/public'))); 
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'))
-})
+app.get('/', require('./routes/root.js'));
+
+app.all('*', (req, res) => {
+  res.status(404); 
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html')); 
+  } else if (req.accepts('json')) {
+    res.json({ 'Error': '404 Not Found' }); 
+  } else {
+    res.type('text').send('404 Not Found');  
+  }
+}); 
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`); 
