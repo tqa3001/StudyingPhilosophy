ee9285a8b381db04f16e8c2acc4ada61e7c9156b
tqa3001 <tqa3001@gmail.com>
2023-04-15 15:57:11 +1000

    refurnished landing page

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -1,20 +1,21 @@
 import { Link } from 'react-router-dom'  /* reloadDocument -> skip client-side routing -> normal a href */
 
 export default function Public() {
+  const landingButtonStyle = "text-white py-2 px-5 rounded-full bg-blue-500 text-center"; // move this to ../styles ?
   return (
-    <section>
-      <header>
-        <h1 className="text-3xl">Welcome</h1>
-        <h2 className="text-xl">Enjoy the ride.</h2>
+    <section className="p-10">
+      <header className="my-5">
+        <h1 className="text-2xl">Welcome to</h1>
+        <h2 className="text-5xl text-blue-500 font-bold">A note-taking app</h2>
       </header>
       <main>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
       </main>
-      <footer> 
-        <Link to="/login">Login</Link> <br /> 
-        <Link to="/dashboard">Open Dashboard</Link> 
+      <footer className="mx-auto w-1/3 mt-10"> 
+        <Link to="/login"><div className={landingButtonStyle + " my-2"}>Login</div></Link> 
+        <Link to="/dashboard"><div className={landingButtonStyle}>Open Dashboard</div></Link> 
       </footer>
     </section>
   )
--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -5,14 +5,18 @@ import MessageBoard from "./MessageQueueViewer";
 export default function Layout() {
   return (
     <div className="h-full">
-      <h1 className="p-3 bg-black text-white">Using Outlets</h1>
+      <div className="flex justify-between bg-black text-white">
+        <h1 className="p-3">Using Outlets</h1>
+        <div>
+          <h2 className="px-3">Frontend port: {FRONTEND_PORT}</h2>
+          <h2 className="px-3">Backend port: {BACKEND_PORT}</h2>
+        </div>
+      </div>
       <Outlet />
       <h1 className="p-3 bg-black text-white fixed bottom-0 right-0">
         Message window
         <MessageBoard />
       </h1>
-      <h2 className="px-3 bg-black text-white">Frontend server port: {FRONTEND_PORT}</h2>
-      <h2 className="px-3 bg-black text-white">Backend server port: {BACKEND_PORT}</h2>
     </div>
   )
 }
